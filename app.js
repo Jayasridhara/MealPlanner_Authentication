3bd7eeff7387b546cff182dd03a0e3f4a09639c8
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-09-21 12:13:38 +0530

    local cors

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const authRouter = require('./routes/authRoutes');
 const cookieParser =require('cookie-parser');
 const morgan=require('morgan');
 const logger = require('./utils/logger');
-
+const cors=require('cors');
 
 const app=express();
 app.use(express.json()); 
 app.use(cookieParser());
-app.use(logger)
+app.use(logger);
+app.use(cors({
+    origin:'http://localhost:5173/'
+}));
 //listen for incomin g requires
 
 app.use('/mealplans',mealplanRouter);
